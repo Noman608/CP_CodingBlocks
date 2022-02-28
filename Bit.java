3db1ed8d3863a4350c98259bbd2f652d1243ee82
Abdullah Al NOMAN <abdullahalnomancse@gmail.com>
2022-02-28 15:09:29 +0530

    Bit file added

---
+++ Bit.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Bit {
+    int getBit(int n ,int i){
+        int mask= (1<<i);
+        int bit = (n & mask) > 0 ? 1:0;
+        return bit;
+    }
+    int UpdateNum(int n,int i){
+        int mask = ~(1<<i);
+        int bit = n & mask;
+        return bit;
+    }
+    int changeAperticularBit(int n,int p,int cng){
+        int mask = (1<<p);
+        return (n & ~mask)|(cng<<p);
+    }
+    int setBit(int n, int i){
+        int mask =(1<<i);
+        int bit = (n | mask);
+        return bit;
+    }
+    int clearBit(int n,int p){
+        int mask = ~0;
+        int q = mask<<p;
+        return (n & q);
+    }
+    int clearItoJ(int n,int i,int j){
+        int mask = ((~0)<<j+1) | (1<<i)-1;
+        return n & mask;
+    }
+    public static void main(String[] args) {
+        BS b = new BS();
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println(b.clearItoJ(31,1,3));
+
+    }
+
+
+}
+
